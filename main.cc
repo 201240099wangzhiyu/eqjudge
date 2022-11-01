b538901a7551db4dd7d32c485bfaf4ff1f783687
201240099-Wang Zhiyu <871428118@qq.com>
2022-11-01 20:32:27 +0800

    finish equal completely

--- main.cc
+++ main.cc
@@ -1,34 +1,73 @@
 #include <iostream>
 #include <filesystem>
 #include <queue>
+#include <vector>
 #include "prog.hh"
+#include "equal.hh"
 using namespace std;
 using namespace std::filesystem;
 int main(int argc, char *argv[]){
+    FILE *abn=fopen("../output/abnormal.csv","w");
+    FILE *equ=fopen("../output/equal.csv","w");
+    FILE *ine=fopen("../output/inequal.csv","w");
+    srand((int)time(0));
     queue<path> type;
-    queue<std::string> file;
+    vector<Equality> pairs;
+    int base=0;
     for (auto& i : filesystem::directory_iterator(argv[1])) {
         type.push(i.path());
-		cout << type.back().string()<< endl;
 	}
-    for (auto& i : filesystem::directory_iterator(type.front())) {
-        file.push(i.path());
-		//cout << file.back()<< endl;
-        //cout<<i.path().stem()<<endl;
-        path path_1(file.back());
-        if(path_1.extension()==".txt"){
-            FILE *fp=fopen(path_1.string().c_str(),"r");
-            char req[1024];
-            fgets(req,1024,fp);
-            cout<<req<<endl;
-            //string cmd="cat "+path_1.string();
-            //system(cmd.c_str());
+    while(!type.empty()){
+        vector<Program> file;
+        char req[1200];
+        for (auto& i : filesystem::directory_iterator(type.front())){
+            path path_1(i.path());
+            if((path_1.extension()==".txt")&&(path_1.stem()=="stdin_format")){
+                FILE *fp=fopen(path_1.string().c_str(),"r");
+                fgets(req,1024,fp);
+            }
         }
-	}
-    //system("g++ ../input/4A/48762087.cpp -o ../input/4A/48762087.out");
-    //system("../input/4A/48762087.out");
-    //system("cat ../input/4A/48762087.cpp");
-    /*while(!type.empty()){
-
-    }*/
+        Randinput input(req);
+        for (auto& i : filesystem::directory_iterator(type.front())){
+            path path_1(i.path());
+            if(path_1.extension()==".cpp"){
+                string name=path_1.string();
+                size_t pos = name.find(".cpp");
+                string runname = name.substr(0, pos);
+                Program prag(path_1.string(),runname,input);
+                if(prag.abnormal()){
+                    fprintf(abn,"%s\n",prag.srcname().c_str());
+                }else{
+                    file.push_back(prag);
+                }
+            }  
+        }
+        for (int i = 0; i < file.size(); i++ ){
+		    for (int j = i+1; j < file.size(); j++ ){
+                Equality pair(file[i],file[j]);
+                pairs.push_back(pair);    
+            }
+	    }
+        for(int i=0;i<3;i++){
+            string* indata=input.getinput();
+            for(int j=0;j<file.size();j++){
+                file[j].run(indata);
+            }
+            for(int j=0;j<pairs.size();j++){
+                pairs[j].judge();
+            }
+        }
+        type.pop();
+        base=pairs.size();
+    }
+    for(int j=0;j<pairs.size();j++){
+        if(pairs[j].isequal()){
+            fprintf(equ,"%s,%s\n",pairs[j].getpro1().srcname().c_str(),pairs[j].getpro2().srcname().c_str());
+        }else if(!pairs[j].isequal()){
+            fprintf(ine,"%s,%s\n",pairs[j].getpro1().srcname().c_str(),pairs[j].getpro2().srcname().c_str());
+        }
+    }
+    fclose(ine);
+    fclose(equ);
+    fclose(abn);
 }
--- prog.hh
+++ prog.hh
@@ -2,22 +2,61 @@
 #define PROGRAM_HH
 
 #include <string>
-
+#include "randinput.hh"
 class Program{
     private:
     std::string _name;
     std::string _run_name;
+    Randinput _input;
     public:
-    Program(std::string name):_name(name){}
-    void run(){
-        std::string cmd1="g++ "+_name+" -o "+_run_name;
-        std::string cmd2=_run_name;
+    Program(std::string name,std::string runname,Randinput input):_name(name),_run_name(runname),_input(input){}
+    void run(std::string arg[1024]){
+        int num=_input.getnum();
+        std::string cmd1="g++ "+_name+" -w -o "+_run_name+".out";
+        std::string cmd2=_run_name+".out"+" < "+_run_name+"input.txt"+" > "+_run_name+"output.txt"+" 2>&1";
+        std::string input="";
+        for(int i=0;i<num;i++){
+            input+=arg[i];
+            input+=" ";
+        }
+        std::string filename=_run_name+"input.txt";
+        FILE *fp=fopen(filename.c_str(),"w+");
+        fprintf(fp,"%s\n",input.c_str());
+        fclose(fp);
+        //std::cout<<input<<filename<<std::endl;
+        //std::cout<<cmd2<<std::endl;
         system(cmd1.c_str());
         system(cmd2.c_str());
+        //std::cout<<std::endl<<"ans"<<std::endl;
+        
+    }
+    bool abnormal(){
+        int num=0;
+        FILE* fp=fopen(_name.c_str(),"r");
+        char ctt[1200];
+        std::string id1=">>";
+        std::string id2="&";
+        while(!feof(fp)){
+            fgets(ctt,1024,fp);
+            std::string origin(ctt);
+            for(int i = 0 ; (i = origin.find(id1,i)) != std::string::npos ;i++ , num ++ );
+            for(int i = 0 ; (i = origin.find(id2,i)) != std::string::npos ;i++ , num ++ );
+        }
+        fclose(fp);
+        if(num!=_input.getnum()){
+            return true;
+        }
+        return false;
     }
     std::string getname(){
+        return _run_name;
+    }
+    std::string srcname(){
         return _name;
-    };
+    }
+    Randinput getinput(){
+        return _input;
+    }
 };
 
 
